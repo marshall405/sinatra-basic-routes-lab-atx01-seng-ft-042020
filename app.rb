d2c79e85db0edbb77478fb839354365d2bde8d1b
marshall405 <marshall.slemp@gmail.com>
2020-05-11 22:08:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Marshall"
+    end
+
+    get '/hometown' do 
+        "My hometown is Covington"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Slow Down"
+    end
+
+
+
 end
